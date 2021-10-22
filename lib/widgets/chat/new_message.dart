da13108fb3d2d61a4d811f7d57c84c7fed8910c1
mekki21 <amr.mekki21@gmail.com>
2021-10-22 13:19:38 +0200

    End

--- lib/widgets/chat/new_message.dart
+++ lib/widgets/chat/new_message.dart
@@ -13,42 +13,21 @@ class _NewMessageState extends State<NewMessage> {
   var _enteredMessage = "";
   final _controller = new TextEditingController();
 
-  FirebaseFirestore? _instance;
-
-  Future<void> getCollectionFromFirebase() async {
-    _instance = FirebaseFirestore.instance;
-    CollectionReference categories = _instance!.collection("users");
-    var user = FirebaseAuth.instance.currentUser;
-
-    DocumentSnapshot snapshot = await categories.doc(user!.uid).get();
-    if (snapshot.exists) {
-      Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
-    }
-  }
-
   void _sendMessage() async {
     FocusScope.of(context).unfocus();
     var user = FirebaseAuth.instance.currentUser;
-    final username = user!.email!.split("@");
-    var snapData;
-    getCollectionFromFirebase();
-    // CollectionReference reference =
-    //     FirebaseFirestore.instance.collection("users");
-    // var now = reference.doc(user!.uid);
-    // var noww = now.path;
+    final userData = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user!.uid)
+        .get();
 
-    // final stream = await FirebaseFirestore.instance
-    //     .collection("/users/bWY1fOepv1UBKhR3xUjdde9ZN9D2")
-    //     .snapshots()
-    //     .first;
     FirebaseFirestore.instance.collection("chat").add(
       {
         "text": _enteredMessage,
         "createdAt": Timestamp.now(),
         "userId": user.uid,
-        "username": username[0],
-        "userImage":
-            "https://firebasestorage.googleapis.com/v0/b/flutter-chat-5957a.appspot.com/o/userImages%2FbWY1fOepv1UBKhR3xUjdde9ZN9D2.jpg?alt=media&token=9cfa4fc0-0261-475e-a78f-de56809454cc"
+        "username": userData.data()!["username"],
+        "userImage": userData.data()!["image_url"],
       },
     );
     _controller.clear();
